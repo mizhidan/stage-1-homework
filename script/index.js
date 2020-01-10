ebf7474ee253e28148ea4eab59c2fc501067faa8
mizhidan <dj9824@126.com>
2020-01-10 11:44:10 +0800

    [danjie] {feat}-{2}: change status color

--- script/index.js
+++ script/index.js
@@ -23,30 +23,49 @@ function showContent(data) {
   if (!Array.isArray(data) && !data instanceof Array) {
     return false;
   }
-  for(let i = 0; i < data.length; ++i) {
-  let taskRow = document.createElement("li");
-  taskRow.setAttribute("class","project-list");
-  let taskContent = `<span class="project-name">${data[i].name}</span>
- <span class="project-description">${data[i].description}</span>
-  <span class="end-time">${data[i].endTime}</span>
- <span class="status">${data[i].status}</span>
+  for (let index = 0; index < data.length; ++index) {
+    let taskRow = document.createElement("li");
+    taskRow.setAttribute("class", "project-list");
+    let taskContent = `<span class="project-name">${data[index].name}</span>
+ <span class="project-description">${data[index].description}</span>
+  <span class="end-time">${data[index].endTime}</span>
+ <span class="status">${data[index].status}</span>
  <span class="option"><button class="delete-btn">删除</button> </span>`;
- taskRow.innerHTML = taskContent;
- newList.appendChild(taskRow);
- adjustWidth(i);
+    taskRow.innerHTML = taskContent;
+    newList.appendChild(taskRow);
+    adjustWidth(index);
+    changeStatusColor(index);
   }
 }
 
 function adjustWidth(index) {
-    let name = document.getElementsByClassName('project-name');
-    let description = document.getElementsByClassName('project-description');
-    let endTime = document.getElementsByClassName('end-time');
-    let status = document.getElementsByClassName('status');
-    let option = document.getElementsByClassName('option');
-    name[index+1].style.width = name[0].offsetWidth + 'px';
-    description[index+1].style.width = description[0].offsetWidth - 10 + 'px';
-    endTime[index+1].style.width = endTime[0].offsetWidth + 'px';
-    status[index+1].style.width = status[0].offsetWidth + 'px';
-    option[index+1].style.width = option[0].offsetWidth + 'px';
+  let name = document.getElementsByClassName("project-name");
+  let description = document.getElementsByClassName("project-description");
+  let endTime = document.getElementsByClassName("end-time");
+  let status = document.getElementsByClassName("status");
+  let option = document.getElementsByClassName("option");
+  name[index + 1].style.width = name[0].offsetWidth + "px";
+  description[index + 1].style.width = description[0].offsetWidth - 10 + "px";
+  endTime[index + 1].style.width = endTime[0].offsetWidth + "px";
+  status[index + 1].style.width = status[0].offsetWidth + "px";
+  option[index + 1].style.width = option[0].offsetWidth + "px";
 }
+
+function changeStatusColor(index) {
+  let status = document.getElementsByClassName("status");
+  switch (status[index + 1].innerHTML) {
+    case "ACTIVE":
+      status[index + 1].style.color = "#666666";
+      break;
+    case "PENDING":
+      status[index + 1].style.color = "#ee706d";
+      break;
+    case "CLOSED":
+      status[index + 1].style.color = "#f7da47";
+      break;
+    default:
+      break;
+  }
+}
+
 taskList();
